69f01dda88e66d776ff9e15336db6093163f16c7
Jazer <contact@jazer.co.uk>
2022-12-13 16:52:44 +0000

    Added member route with environment variables

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
-const PORT = 4000;
+require('dotenv').config();
+
+const PORT = process.env.PORT || 4000;
+
 const express = require('express');
 
 const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+
+const memberRouter = require('./routes/members/memberRouter');
+app.use('/members', memberRouter);
 
 app.get('/', (req, res) => {
     res.status(200).json({
