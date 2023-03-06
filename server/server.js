c8df12f17be7b46b46c597399d7f0321bd5a9898
Jazer <contact@jazer.co.uk>
2023-03-06 10:23:10 +0000

    Added routes to server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,54 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
 
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
-app.use(express.json()); 
+const cors = require("cors");
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const memberRouter = require('./routes/members/memberRouter');
-app.use('/members', memberRouter);
+const loginRouter = require("./routes/login/loginRouter");
+app.use("/login", loginRouter);
 
-app.get('/', (req, res) => {
+const memberRouter = require("./routes/members/memberRouter");
+app.use("/members", memberRouter);
+
+const paymentMethodsRouter = require("./routes/paymentMethods/paymentMethodsRouter");
+app.use("/paymentMethods", paymentMethodsRouter);
+
+const paymentRouter = require("./routes/payments/paymentRouter");
+app.use("/payments", paymentRouter);
+
+const lessonRouter = require("./routes/lessons/lessonRouter");
+app.use("/lessons", lessonRouter);
+
+const lessonTypesRouter = require("./routes/lessonTypes/lessonTypesRouter");
+app.use("/lessonTypes", lessonTypesRouter);
+
+const lessonPricingRouter = require("./routes/lessonPricing/lessonPricingRouter");
+app.use("/lessonPricing", lessonPricingRouter);
+
+const lessonPurchaseTypesRouter = require("./routes/lessonPurchaseTypes/lessonPurchaseTypesRouter");
+app.use("/lessonPurchaseTypes", lessonPurchaseTypesRouter);
+
+const lessonPurchasesRouter = require("./routes/lessonPurchases/lessonPurchasesRouter");
+app.use("/lessonPurchases", lessonPurchasesRouter);
+
+const attendanceRouter = require("./routes/attendance/attendanceRouter");
+app.use("/attendance", attendanceRouter);
+
+app.get("/", (req, res) => {
     res.status(200).json({
-        message: "Ok"
+        message: "Ok",
     });
 });
 
